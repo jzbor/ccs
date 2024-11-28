b83350814c62d42474818fce36a451463f2894e1
jzbor <jzbor@posteo.net>
2024-11-28 22:50:37 +0100

    Adding doubly linked, reference counted list

---
+++ src/bisimilarity/list.rs
@@ -0,0 +1,78 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct ListItem<T> {
+    previous: Option<RcItem<T>>,
+    next: Option<RcItem<T>>,
+    data: T,
+}
+
+pub type RcItem<T> = Rc<RefCell<ListItem<T>>>;
+
+pub struct RcList<T> {
+    first: Option<RcItem<T>>,
+    last: Option<RcItem<T>>,
+    size: usize
+}
+
+pub struct RcListIterator<T> {
+    current: Option<RcItem<T>>,
+}
+
+impl<T> RcList<T> {
+    pub fn new() -> Self {
+        RcList {
+            first: None,
+            last: None,
+            size: 0,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.size
+    }
+
+    pub fn put(&mut self, data: T) {
+        let previous_last = self.last.take();
+        let item = Rc::new(RefCell::new(ListItem {
+            previous: previous_last,
+            next: None,
+            data
+        }));
+        self.size += 1;
+        self.last = Some(item);
+    }
+
+    pub fn remove(&mut self, item: RcItem<T>) -> T {
+        let mut item_ref = item.borrow_mut();
+        let previous_opt = item_ref.previous.take();
+        let next_opt = item_ref.next.take();
+        drop(item_ref);
+        if let Some(previous) = &previous_opt {
+            previous.borrow_mut().next = next_opt.clone();
+        }
+        if let Some(next) = &next_opt {
+            next.borrow_mut().previous = previous_opt;
+        }
+        Rc::into_inner(item).unwrap()
+            .into_inner().data
+    }
+
+    pub fn iter(&self) -> RcListIterator<T> {
+        RcListIterator { current: self.first.clone() }
+    }
+}
+
+impl<T> Iterator for RcListIterator<T> {
+    type Item = RcItem<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.current.take() {
+            Some(current) => {
+                self.current = current.borrow().next.clone();
+                Some(current)
+            },
+            None => None,
+        }
+    }
+}
